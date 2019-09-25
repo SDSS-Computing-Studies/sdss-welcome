67d130097662414edcfcf0b3e341b592f7955a11
hungrybeagle <copper@hungrybeagle.com>
2019-09-25 11:03:22 -0700

    lesson 2 modifications

---
+++ 02.prebuilt.functions.js
@@ -0,0 +1,25 @@
+function instructions() {
+    console.log("Enter in a base and an exponent")
+    console.log("use base = 'value'")
+    console.log("use exponent = 'value'")
+}
+
+
+/*
+example of using the Math.pow function to work with exponents
+
+let base = 4;
+let exponent = 1.5;
+let answer = Math.pow(base,exponent);
+
+let output = base + " to the power of " + exponent + " is " + answer;
+console.log(output);
+
+*/
+
+let x = 13.9999;
+
+let rounded = Math.round(x);
+
+console.log(rounded);
+
