e45f1f5b0be8ec722ea673ceb50acdeb2f05cef8
hungrybeagle <copper@hungrybeagle.com>
2019-12-02 09:57:43 -0800

    Create 12.arrayUses.js

---
+++ 12.arrayUses.js
@@ -0,0 +1,85 @@
+/*
+One of the important things you can do with an array is sort them. 
+Imagine having arrays that contain information for a classroom. 
+You might want to sort the students into alphabetical order and 
+redisplay them. More advanced programs might be able to sort 
+students by Firstname, or Lastname, or Student Number or grade!
+
+There are many different ways to sort an array. One way is the Bubble Sort:
+*/
+// Bubble Sort
+// A way to sort the values in an array
+
+console.log("The bubbtle sort!");
+console.log("=================");
+
+function displayArray(inputArray) {
+    let output = "";
+    for (let i = 0; i < inputArray.length; i++) {
+        output = output + " " + inputArray[i];
+    }
+    return output;
+}
+var numbers = [10,30,15,19,20,10,4,2,40,31,22];
+let output = displayArray(numbers);
+console.log("before" + output)
+
+var end = numbers.length;
+for (var i = 0; i < end; i++) {
+	for (var j = i; j < end; j++) {
+  	if (numbers[i] > numbers[j]) {
+    	var temp = numbers[i];
+      numbers[i] = numbers[j];
+      numbers[j] = temp
+    }
+  }
+}
+
+output = displayArray(numbers)
+console.log("after: " + output);
+
+/*
+
+Last class, we saw that you could add elements to the end of an array 
+using the array.push(i) method/function. This adds the element "i" 
+to the end of the array. However, if you can add an element, you 
+can also delete an element:
+*/
+
+// Array Splice
+// array.splice( position, howMany)
+// deletes the element at a certain position
+// removes howMany elements
+// Note: array.splice can also be used to insert items
+
+console.log("")
+console.log("Deleting from an Array")
+console.log("======================")
+
+
+var numbers2 = [10,30,15,19,20,10,4,2,40,31,22];
+output = displayArray(numbers2)
+console.log("before:" + output)
+
+var x = numbers2.splice(1,1);
+// this should remove the "30" at the second position
+// and store that value into x
+console.log("x:" + x);
+
+output = displayArray(numbers2)
+console.log("after splice:" + output)
+
+
+/*
+Assignment:
+Create a shuffled deck of cards:
+Steps:
+1. Assign cards to an array of 52 elements
+2. Create a temporary array that is a copy of your unshuffled deck. This allows you to work with the deck elements without deleting/modifying your original deck.
+3. Select a card at random from the temp array. Remove it from the existing array and add it to a new array that represents your shuffled deck.
+4. Repeat this until you have no more cards in your original array.
+5. Display your shuffled deck.
+
+Bonus:
+Deal 5 cards to 4 different people and display what each person has.
+*/
